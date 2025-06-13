c9b8bc883c5765f1d19e05394c1a5f4f09297c98
byteAndBait <192332038+byteAndBait@users.noreply.github.com>
2025-06-13 08:19:50 +0300

    feat: Add backspace functionality

--- main.js
+++ main.js
@@ -1,30 +1,8 @@
 /* 
-we need to make a typing test that measures you WPM
-we have .text that has the text
-we have .keyboard that has the qwerty and the preview
-the text will be first converted into spans
-we have three classes .correct .false .input
-the .input class will be on the first span
-
-first of all when input starts a timer start counting and when the last letter is .correct the time is up
-on type check if there is a timer or not to avoid multiple timers
-when keydown checks if the span the has the .input class its textcontent the same as the key
-    if right 
-        remove .false (when correcting the letter if typed wrong)
-        it takes .correct
-        remove .input 
-        pass .input to the next span using nextElmentSibling
-    if false
-        it takes .false
-every span in .keyboard has a data-key
-data-key == key then give that element .typed class
-and when keyup remove that class
-
-
 Bugs to be fixed
     - 
 Features to be Added
-    - Backspace
+    - keyboard preview
 */
 
 const modifiers = ["Shift", "CapsLock" , "Tab" , "Control", "Alt", "Meta"]
@@ -46,7 +24,11 @@ let timer;
 document.addEventListener("keydown", function main(e) {
   let input = textElement.querySelector(".input");
   let key = e.key;
-console.log(key)
+  console.log(key)
+  if(modifiers.includes(key)){
+    e.preventDefault()
+    return
+  }
   // Timer
   if (!didTimerStart) {
     timer = setInterval(() => {
@@ -56,6 +38,21 @@ console.log(key)
     didTimerStart = true;
     console.log("timer started");
   }
+
+  // Backspace
+  if(key == "Backspace"){
+    if(input.previousElementSibling){
+      input.previousElementSibling.classList.add("input")
+      input.previousElementSibling.classList.remove("correct")
+      input.previousElementSibling.classList.remove("false")
+      input.classList.remove("input")
+    }
+    
+    e.preventDefault()
+    return
+  }
+
+
 
   // actual process
   if (key == input.textContent) {
@@ -74,9 +71,11 @@ console.log(key)
       console.log(text.length / time * 10 + "WPM");
     }
   } else {
-    if(!modifiers.includes(key)){
+      input.classList.remove("input")
         input.classList.add("false");
-    }
+        if (input.nextElementSibling) {
+          input.nextElementSibling.classList.add("input");
+        }
   }
 
 });
