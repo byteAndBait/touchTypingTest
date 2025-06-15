898b1258dd1c29ef07a229b6006da40751cb44f0
byteAndBait <192332038+byteAndBait@users.noreply.github.com>
2025-06-15 08:27:40 +0300

    feat: Add the functionality to add custom text

--- main.js
+++ main.js
@@ -6,36 +6,42 @@ Features to be Added
     - add your own text
 */
 
-const modifiers = ["Shift", "CapsLock" , "Tab" , "Control", "Alt", "Meta"]
+const modifiers = ["Shift", "CapsLock" , "Tab" , "Control", "Alt", "Meta","F1","F2","F3","F4","F5","F6","F7","F8","F9","F10","F11","F12","Escape"]
 let keyboard = document.querySelector(".keyboard");
-let text = `a b c d e f g h i j k l m o p q r s t u v w x y z`
-let convertedText = text
-  .split("")
-  .map((a) => (a = `<span>${a}</span>`))
-  .join("");
-let textElement = document.querySelector(".text");
-textElement.innerHTML = convertedText;
-textElement.firstChild.classList.add("input");
-textElement.lastChild.classList.add("last");
+let convertedText;
+let textElement ;
 let time = 0;
 let didTimerStart = false;
 let timerElement = document.querySelector(".timer");
 let timer;
+let isListenerBlocked = false;
+function handleCustomText(text){
+   convertedText = text
+    .split("")
+    .map((a) => (a = `<span>${a}</span>`))
+    .join("");
+    textElement = document.querySelector(".text");
+  textElement.innerHTML = convertedText;
+  textElement.firstChild.classList.add("input");
+  textElement.lastChild.classList.add("last");
+}
+
+handleCustomText(`a b c d e f g h i j k l m o p q r s t u v w x y z`)
+
 
 document.addEventListener("keydown", function main(e) {
   let input = textElement.querySelector(".input");
   let key = e.key;
-
-  // keyboard preview
-  keyboard.querySelector(`span[data-key="${e.code}"]`).classList.add("active")
-  if(key == "Shift"){
-    
-  }
-
-  if(modifiers.includes(key)){
-    e.preventDefault()
+  console.log(key)
+  if(isListenerBlocked || modifiers.includes(key)){
+    console.log("listener is blocked")
     return
   }
+  // keyboard preview
+  keyboard.querySelector(`span[data-key="${e.code}"]`).classList.add("active")
+
+
+ 
 
   // Timer
   if (!didTimerStart) {
@@ -79,27 +85,41 @@ document.addEventListener("keydown", function main(e) {
         }
   }
   if (input.classList.contains("last")) {
-    document.removeEventListener("keydown", main);
-    document.removeEventListener("keyup", main);
+    isListenerBlocked = true
     clearInterval(timer)
-    console.log(text.length / time * 10 + "WPM");
+    console.log(convertedText.length / time * 10 + "WPM");
   }
 });
 
 document.addEventListener("keyup", function main(e) {
+  if(keyboard.querySelector(`span[data-key="${e.code}"]`)){
   keyboard.querySelector(`span[data-key="${e.code}"]`).classList.remove("active")
+
+  }
 })
 
 
 
 // Adding custom text
 let customTextContainer = document.querySelector(".customTextContainer")
-let customTextWrapper = document.querySelector("customTextWrapper") 
+let customTextWrapper = document.querySelector(".customTextWrapper") 
+let addCustomTextButton = document.querySelector("button.addCustomText")
+let addCustomTextOk = document.querySelector(".customTextOk") 
+let addCustomTextNo = document.querySelector(".customTextNo") 
+let customTextInput = document.querySelector(".customTextContainer #customTextInput")
 document.addEventListener("click", (e)=>{
   if(e.target.classList.contains("custom")){
+    isListenerBlocked = true
     e.preventDefault()
+  }else if(e.target == addCustomTextButton){
+    customTextContainer.style.cssText = "display:block" 
+    customTextWrapper.style.cssText = "display:block;"
   }else{
     customTextContainer.style.cssText = "display:none" 
     customTextWrapper.style.cssText = "display:none;"
+  }
+  if(e.target == addCustomTextOk){
+    isListenerBlocked= false;
+    handleCustomText(customTextInput.value)
   }
 })
